5463a49f9ab66c865938588ffb386f605d86b1c7
bukhari-work <ahmadbukharkwork@gmail.com>
2026-05-05 23:34:50 +0800

    FIX: update db conenction pool

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -6,20 +6,31 @@ if (!env.POSTGRES_URL) {
 	throw new Error("POSTGRES_URL environment variable is missing");
 }
 
+let connectionString = env.POSTGRES_URL;
+
+// Safer URL handling
+if (!dev) {
+	const url = new URL(connectionString);
+	url.searchParams.set("pgbouncer", "true");
+	connectionString = url.toString();
+}
+
 declare global {
 	var __postgres: ReturnType<typeof postgres> | undefined;
 }
 
 const sql =
 	globalThis.__postgres ??
-	postgres(env.POSTGRES_URL, {
+	postgres(connectionString, {
 		max: dev ? 10 : 1,
 
 		// IMPORTANT: must be false in production for Supavisor
 		prepare: dev,
 
-		idle_timeout: 20,
-		connect_timeout: 10,
+		idle_timeout: dev ? 20 : 1,
+		connect_timeout: 5,
+
+		ssl: !dev ? "require" : undefined,
 	});
 
 if (dev) {
